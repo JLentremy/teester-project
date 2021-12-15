9f11e8914b85e47b1db5bdda8985939922a1b3ab
JLentremy <jeremy@yada.email>
2021-12-15 13:22:49 +0100

    AppContext

--- src/app/AppContext.tsx
+++ src/app/AppContext.tsx
@@ -1,5 +1,57 @@
-const AppContext = () => {
-  return;
+import { DateTime } from "luxon";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { ProfileProps } from "../components/Profile/types";
+
+type AppContextType = {
+  profile: ProfileProps | undefined;
+  getProfile: () => void;
 };
 
-export default AppContext;
+const AppContext = React.createContext<AppContextType>({
+  profile: undefined,
+  getProfile: () => {
+    // initially empty
+  },
+});
+
+export const AppProvider: React.FunctionComponent = ({ children }) => {
+  const [profile, setProfile] = useState<ProfileProps | undefined>();
+  const getProfile = useCallback(() => {
+    const data = {
+      firstname: "Maxime",
+      lastname: "Baconnais",
+      birthdate: DateTime.local(1996, 7, 23),
+      address: "7 ruelle Lilly Reich",
+      city: "Nantes",
+      country: "",
+      contact_mail: "maxime.baconnais@teester.com",
+      login_mail: "maxime.baconnais@teester.com",
+      phone: "+33658729674",
+    };
+    setProfile(data);
+  }, []);
+
+  useEffect(() => {
+    getProfile();
+  }, [getProfile]);
+
+  const MemoValue = useMemo(
+    () => ({
+      profile,
+      getProfile,
+    }),
+    [profile, getProfile]
+  );
+
+  return (
+    <AppContext.Provider value={MemoValue}>{children}</AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => useContext(AppContext);
